203114ebbb2a822fae42c7155731b06a85193433
EfremovAS <Alexef3@mail.ru>
2024-05-15 19:41:38 +0300

    container fixes, added ComposedSendeService

--- core/apps/customers/services/senders.py
+++ core/apps/customers/services/senders.py
@@ -1,4 +1,6 @@
 from abc import ABC
+from dataclasses import dataclass
+from typing import Iterable
 
 from core.apps.customers.entities.entities import CustomerEntity
 
@@ -10,3 +12,22 @@ class BaseSenderService(ABC):
 class DummySenderService(BaseSenderService):
     def send_code(self, customer: CustomerEntity, code: str) -> None:
         print(f"Code to user: {customer}:, sent: {code}")
+
+
+class EmailSenderService(BaseSenderService):
+    def send_code(self, customer: CustomerEntity, code: str) -> None:
+        print(f"Code code {code} to user: customer_email")
+
+
+class PushSenderService(BaseSenderService):
+    def send_code(self, customer: CustomerEntity, code: str) -> None:
+        print(f"Code sent code {code} to user in push notification")
+
+
+@dataclass
+class ComposedSenderService(BaseSenderService):
+    sender_services: Iterable[BaseSenderService]
+
+    def send_code(self, customer: CustomerEntity, code: str) -> None:
+        for service in self.sender_services:
+            service.send_code(customer, code)
